5d287a052a35c29856f22f67bf681d585569b70f
autumn-the-kitty-cat <147276465+autumn-the-kitty-cat@users.noreply.github.com>
2025-11-09 22:11:43 -0500

    reformat home-manager/programs/nvim/options.nix

--- home-manager/programs/nvim/options.nix
+++ home-manager/programs/nvim/options.nix
@@ -1,57 +1,58 @@
-{...}: {
-    programs.nixvim = {
-        autoCmd = [
-            {
-                event = [ "VimLeave" ];
-                command = ":set guicursor=a:ver90-blinkon0";
-            }
-        ];        
-        diagnostic.settings = {
-            virtual_lines = {
-                current_line = true;
-            };
-            float = {
-                border = "rounded";
-                source = "always";
-            };
-        };
-        opts = {
-            shell = "nu";
-            
-            fillchars = "eob: ";
-
-            number = true;
-            relativenumber = true;
-
-            tabstop = 4;
-            softtabstop = 4;
-            showtabline = 4;
-            shiftwidth = 4;
-            expandtab = true;
-            smartindent = true;
-            breakindent = true;
-
-            hlsearch = true;
-            incsearch = true;
-            ignorecase = true;
-            smartcase = true;
-
-            splitbelow = true;
-            splitright = true;
-
-            mouse = "a";
-
-            updatetime = 50;
-
-            swapfile = false;
-            backup = false;
-            undofile = true;
-
-            scrolloff = 10;
-
-            cursorline = true;
-
-            wildmenu = true;
-        };
+{ ... }:
+{
+  programs.nixvim = {
+    autoCmd = [
+      {
+        event = [ "VimLeave" ];
+        command = ":set guicursor=a:ver90-blinkon0";
+      }
+    ];
+    diagnostic.settings = {
+      virtual_lines = {
+        current_line = true;
+      };
+      float = {
+        border = "rounded";
+        source = "always";
+      };
     };
-       }
+    opts = {
+      shell = "nu";
+
+      fillchars = "eob: ";
+
+      number = true;
+      relativenumber = true;
+
+      tabstop = 4;
+      softtabstop = 4;
+      showtabline = 4;
+      shiftwidth = 4;
+      expandtab = true;
+      smartindent = true;
+      breakindent = true;
+
+      hlsearch = true;
+      incsearch = true;
+      ignorecase = true;
+      smartcase = true;
+
+      splitbelow = true;
+      splitright = true;
+
+      mouse = "a";
+
+      updatetime = 50;
+
+      swapfile = false;
+      backup = false;
+      undofile = true;
+
+      scrolloff = 10;
+
+      cursorline = true;
+
+      wildmenu = true;
+    };
+  };
+}
