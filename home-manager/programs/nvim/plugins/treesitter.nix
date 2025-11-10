80e337f025a462f3ea1578731eb2b86b3936edb6
autumn-the-kitty-cat <147276465+autumn-the-kitty-cat@users.noreply.github.com>
2025-11-09 22:11:16 -0500

    reformat home-manager/plugins/nvim/plugins/treesitter.nix

--- home-manager/programs/nvim/plugins/treesitter.nix
+++ home-manager/programs/nvim/plugins/treesitter.nix
@@ -1,13 +1,10 @@
-{pkgs, ...}:
+{ ... }:
 {
-    programs.nixvim.plugins.treesitter = {
-        enable = true;
-        settings = {
-            auto_install = true;
-            highlight.enable = true;
-        };
-#        grammarPackages = [
-#            pkgs.vimPlugins.nvim-treesitter.builtGrammars.nu
-#        ];
+  programs.nixvim.plugins.treesitter = {
+    enable = true;
+    settings = {
+      auto_install = true;
+      highlight.enable = true;
     };
+  };
 }
